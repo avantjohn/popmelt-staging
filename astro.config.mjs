367c14794c3f50d05f8f0d4b112da44ae0257a32
John Jones <john@popmelt.com>
2025-01-31 14:51:41 -0600

    init commit

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import icon from 'astro-icon';
+
+// https://astro.build/config
+export default defineConfig({
+  devToolbar: {
+    enabled: false
+},
+  integrations: [icon()]
+});
